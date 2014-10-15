59a1ccee3a0a549ebc97d3d1795a55fcfa681f47
Rich Rines <rich.rines@me.com>
2014-10-15 13:53:28 -0700

    Refactor FB ObjectHelpers and Corresponding Spec
    https://trello.com/c/5lEDaHPr

--- lib/zuck/fb_object/error.rb
+++ lib/zuck/fb_object/error.rb
@@ -1,8 +1,12 @@
 module Zuck
   class ZuckError < StandardError; end
   module Error
-    class ReadOnly < ::Zuck::ZuckError; end
-    class MissingToken < ::Zuck::ZuckError; end
+    class CreateConnectionsFailed < ::Zuck::ZuckError; end
+    class DeleteRequestFailed < ::Zuck::ZuckError; end
+    class GetRequestFailed < ::Zuck::ZuckError; end
     class InvalidClass < ::Zuck::ZuckError; end
+    class MissingToken < ::Zuck::ZuckError; end
+    class PostRequestFailed < ::Zuck::ZuckError; end
+    class ReadOnly < ::Zuck::ZuckError; end
   end
 end
